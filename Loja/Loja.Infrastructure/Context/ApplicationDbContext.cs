d73bf4b2d4806adb10456347c7a0e2c3db2459bd
daniellfranco <daniel.luis.franco@gmail.com>
2023-05-19 12:44:11 -0300

    feat: Cria DbContext para o EFCore

---
+++ Loja/Loja.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Loja.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Loja.Infrastructure.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Vendedor> Vendedores { get; set; }
+        public DbSet<Ordem> Ordens {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext)
+                .Assembly);
+        }
+    }
+}
