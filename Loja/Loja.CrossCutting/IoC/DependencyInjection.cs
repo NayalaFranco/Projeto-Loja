93d84f739c06c92fbb270fa6741900514de889b7
daniellfranco <daniel.luis.franco@gmail.com>
2023-05-23 12:34:01 -0300

    feat: Configura injeção de dependencia de serviços

---
+++ Loja/Loja.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using Loja.Application.Interfaces;
+using Loja.Application.Mappings;
+using Loja.Application.Services;
+using Loja.Domain.Interfaces;
+using Loja.Infrastructure.Context;
+using Loja.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Loja.CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IClienteService, ClienteService>();
+
+            services.AddScoped<IVendedorRepository, VendedorRepository>();
+            services.AddScoped<IVendedorService, VendedorService>();
+
+            services.AddScoped<IOrdemRepository, OrdemRepository>();
+            services.AddScoped<IOrdemClienteService, OrdemClienteService>();
+            services.AddScoped<IOrdemVendedorService, OrdemVendedorService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
