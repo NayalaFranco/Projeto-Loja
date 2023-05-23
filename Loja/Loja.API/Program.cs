b3cc47f2d57b45d6317deb729a46c475122744a7
daniellfranco <daniel.luis.franco@gmail.com>
2023-05-23 14:55:14 -0300

    feat: Adiciona os serviços e configura json options.

--- Loja/Loja.API/Program.cs
+++ Loja/Loja.API/Program.cs
@@ -1,8 +1,21 @@
+using System.Text.Json.Serialization;
+using Loja.CrossCutting.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions
+            .Converters.Add(new JsonStringEnumConverter());
+    });        
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
