e397be90310ed23ff38ef9d44cf62594539f9616
daniellfranco <daniel.luis.franco@gmail.com>
2023-06-29 08:56:58 -0300

    feat: Adiciona Descricao ao DTO devido a refatoração do Domain

--- Loja/Loja.Application/DTOs/CategoriaDTO.cs
+++ Loja/Loja.Application/DTOs/CategoriaDTO.cs
@@ -6,12 +6,17 @@ namespace Loja.Application.DTOs
 {
     public class CategoriaDTO
     {
-        public int Id { get; }
+        public int Id { get; set; }
 
         [Required(ErrorMessage = "Informe o nome da categoria")]
         [MinLength(3)]
         [MaxLength(100)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe uma descrição para a categoria")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Descricao { get; set; }
 
         [Required(ErrorMessage = "Informe um Url para a imagem")]
         [MinLength(5)]
