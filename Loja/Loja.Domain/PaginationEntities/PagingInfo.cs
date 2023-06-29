80ba45990ab2d422055e1331120f167507145113
daniellfranco <daniel.luis.franco@gmail.com>
2023-06-29 08:24:15 -0300

    feat: Adiciona entidades para efetuar a paginação dos requests.

---
+++ Loja/Loja.Domain/PaginationEntities/PagingInfo.cs
@@ -0,0 +1,31 @@
+﻿namespace Loja.Domain.PaginationEntities
+{
+    /// <summary>
+    /// Objeto para carregar dados de paginação
+    /// </summary>
+    public class PagingInfo
+    {
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+
+        public bool HasPrevious => CurrentPage > 1;
+        public bool HasNext => CurrentPage < TotalPages;
+
+        /// <summary>
+        /// Cria objeto com os dados de paginação
+        /// </summary>
+        /// <param name="count">Valor total de itens do banco de dados</param>
+        /// <param name="pageNumber">Numero da página</param>
+        /// <param name="pageSize">Quantidade de itens por página</param>
+        public PagingInfo(int count, int pageNumber, int pageSize)
+        {
+            TotalCount = count;
+            PageSize = pageSize;
+            CurrentPage = pageNumber;
+            // Lógica de arredondamento
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+        }
+    }
+}
---
+++ Loja/Loja.Domain/PaginationEntities/PagingParameters.cs
@@ -0,0 +1,25 @@
+﻿namespace Loja.Domain.PaginationEntities
+{
+    /// <summary>
+    /// Objeto com os parâmetros para efetuar a paginação
+    /// </summary>
+    public class PagingParameters
+    {
+        // Altere para definir o limite do máximo por pagina
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+        public string OrderedBy { get; set; } = "id";
+    }
+}
