f26caf9eaf9a4f35c72e23166f116405f46222b1
daniellfranco <daniel.luis.franco@gmail.com>
2023-06-29 08:50:06 -0300

    refactor: Altera nome propriedade cpf.

--- Loja/Loja.Infrastructure/EntitiesConfiguration/ClienteConfiguration.cs
+++ Loja/Loja.Infrastructure/EntitiesConfiguration/ClienteConfiguration.cs
@@ -10,7 +10,7 @@ namespace Loja.Infrastructure.EntitiesConfiguration
         {
             builder.HasKey(x => x.Id);
             builder.Property(p => p.Nome).HasMaxLength(128).IsRequired();
-            builder.Property(p => p.CPF).HasMaxLength(14).IsRequired();
+            builder.Property(p => p.Cpf).HasMaxLength(14).IsRequired();
             builder.Property(p => p.Nascimento).IsRequired();
             builder.Property(p => p.Endereco).HasMaxLength(128);
             builder.Property(p => p.Email).HasMaxLength(128);
