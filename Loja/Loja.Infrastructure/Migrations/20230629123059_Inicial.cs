b134d4858ec2ba0dcabf6120ca70eca996c9a907
daniellfranco <daniel.luis.franco@gmail.com>
2023-06-29 10:08:04 -0300

    chore: Cria migrations com dados de exemplo.

---
+++ Loja/Loja.Infrastructure/Migrations/20230629123059_Inicial.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loja.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImagemUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Nascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vendedores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Nascimento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(128)", maxLength: 128, nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vendedores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    ImagemUrl = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false, defaultValue: 1),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ordens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VendedorId = table.Column<int>(type: "int", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    StatusVenda = table.Column<int>(type: "int", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ordens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ordens_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ordens_Vendedores_VendedorId",
+                        column: x => x.VendedorId,
+                        principalTable: "Vendedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrdemProdutoJuncao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrdemId = table.Column<int>(type: "int", nullable: false),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    NomeProduto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    Desconto = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdemProdutoJuncao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrdemProdutoJuncao_Ordens_OrdemId",
+                        column: x => x.OrdemId,
+                        principalTable: "Ordens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrdemProdutoJuncao_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdemProdutoJuncao_OrdemId",
+                table: "OrdemProdutoJuncao",
+                column: "OrdemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrdemProdutoJuncao_ProdutoId",
+                table: "OrdemProdutoJuncao",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordens_ClienteId",
+                table: "Ordens",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ordens_VendedorId",
+                table: "Ordens",
+                column: "VendedorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdemProdutoJuncao");
+
+            migrationBuilder.DropTable(
+                name: "Ordens");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Vendedores");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ Loja/Loja.Infrastructure/Migrations/20230629123303_PopulaDbExemplo.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Loja.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaDbExemplo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            // Categorias
+            mb.Sql("INSERT INTO Categorias(Nome,Descricao,ImagemUrl) " +
+                "VALUES('Material Escolar','Materiais escolar diversos','materialEscolarCat.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome,Descricao,ImagemUrl) " +
+                "VALUES('Roupas','Roupas Masculinas e Femininas','roupasCat.jpg')");
+            mb.Sql("INSERT INTO Categorias(Nome,Descricao,ImagemUrl) " +
+                "VALUES('Eletrodomésticos','Eletrodomésticos para a sua casa','eletrodomesticosCat.jpg')");
+
+            // Produtos
+            // Cat. Mat. Escolar
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Caderno espiral','Caderno espiral 100 folhas',7.45,'caderno1.jpg',50,'2023-05-02',1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Estojo escolar','Estojo escolar cinza',5.65,'estojo1.jpg',70,'2023-04-02',1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Borracha escolar','Borracha branca pequena',3.25,'borracha1.jpg',80,'2023-03-01',1)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Calculadora escolar','Calculadora simples',15.39,'calculadora1.jpg',20,'2023-04-29',1)");
+
+            // Cat. Roupas
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Camiseta Polo Masc.','Camiseta Polo Masc. Tam.M',56.90,'camisetaPolo1.jpg',11,'2023-05-01',2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Jaqueta Unissex','Jaqueta Unissex Tam.G',128.90,'jaquetaUnissex1.jpg',4,'2023-02-11',2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Calça Jeans Fem.','Calça Jeans Fem. Azul Tam.P',69.90,'jeans1.jpg',20,'2023-04-10',2)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Pct. c/ 5 Meias','Pct. c/ 5 meias cores diversas Tam.43',21.90,'pacoteMeia1.jpg',25,'2022-10-11',2)");
+
+            // Cat. Eletrodomesticos
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Geladeira','Geladeira 110v Branca',999.90,'geladeira2.jpg',8,'2023-05-03',3)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Batedeira','Batedeira 220v Verm.',259.90,'batedeiraVerm1.jpg',3,'2023-03-20',3)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Maquina de Lavar','Maquina de Lavar 110v Branca',1990.00,'maqLavar.jpg',5,'2023-10-11',3)");
+
+            mb.Sql("INSERT INTO Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId) " +
+            "VALUES('Cooktop Elétrico','Cooktop elétrico 220v',2890.90,'coocktop1.jpg',11,'2023-01-02',3)");
+
+            // Vendedores
+            mb.Sql("INSERT INTO Vendedores(Nome,Cpf,Nascimento,Endereco,Email,Telefone,DataCadastro) " +
+                "VALUES('Genivaldo Jovaldo','565.703.380-99','1990-08-20','R.Endereço qualquer. N.00','email@email.com','(00) 0-0000-0000','2022-02-28')");
+
+            mb.Sql("INSERT INTO Vendedores(Nome,Cpf,Nascimento,Endereco,Email,Telefone,DataCadastro) " +
+                "VALUES('Josenildo Gevildo','380.751.090-74','1978-01-11','R.Endereço qualquer. N.01','email@email.com','(01) 2-3456-7891','2023-03-23')");
+
+            // Clientes
+            mb.Sql("INSERT INTO Clientes(Nome,Cpf,Nascimento,Endereco,Email,Telefone,DataCadastro) " +
+                "VALUES('Amado Amoroso','026.122.460-37','1978-01-11','R.Endereço qualquer. N.02','email@email.com','(01) 2-3456-7891','2023-08-23')");
+
+            mb.Sql("INSERT INTO Clientes(Nome,Cpf,Nascimento,Endereco,Email,Telefone,DataCadastro) " +
+                "VALUES('Chaplin da Silva','218.461.780-61','1999-06-12','R.Endereço qualquer. N.04','email@email.com','(11) 1-1111-1111','2021-01-11')");
+
+            // Ordens
+            mb.Sql("INSERT INTO Ordens(VendedorId,ClienteId,Total,StatusVenda,DataCriacao) " +
+                "VALUES(1,1,5526.63,3,'2023-06-03')");
+
+            mb.Sql("INSERT INTO Ordens(VendedorId,ClienteId,Total,StatusVenda,DataCriacao) " +
+                "VALUES(2,1,164.06,0,'2023-05-09')");
+
+            mb.Sql("INSERT INTO Ordens(VendedorId,ClienteId,Total,StatusVenda,DataCriacao) " +
+                "VALUES(2,2,16.35,4,'2023-06-03')");
+
+            // OrdemProduto - Tabela de Junção
+            // Ordem - 1
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(1,9,'Geladeira 110v Branca',999.90,1,99.99)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(1,10,'Batedeira 220v Verm.',259.90,1,25.99)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(1,11,'Maquina de Lavar 110v Branca',1990.00,1,199.00)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(1,12,'Cooktop elétrico 220v',2890.90,1,289.09)");
+
+            // Ordem - 2
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(2,8,'Pct. c/ 5 meias cores diversas Tam.43',21.90,2,2.19)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(2,6,'Jaqueta Unissex Tam.G',128.90,1,6.45)");
+
+            // Ordem - 3
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(3,1,'Caderno espiral 100 folhas',7.45,1,0)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(3,2,'Estojo escolar cinza',5.65,1,0)");
+
+            mb.Sql("INSERT INTO OrdemProdutoJuncao(OrdemId,ProdutoId,NomeProduto,PrecoUnitario,Quantidade,Desconto) " +
+                "VALUES(3,3,'Borracha branca pequena',3.25,1,0)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("DELETE FROM Categorias");
+            mb.Sql("DELETE FROM Produtos");
+            mb.Sql("DELETE FROM Vendedores");
+            mb.Sql("DELETE FROM Clientes");
+            mb.Sql("DELETE FROM Ordens");
+            mb.Sql("DELETE FROM OrdemProdutoJuncao");
+        }
+    }
+}
